efcae1be88428b4268a75d17949282dae7a5d82a
firnsan <firnsan@gmail.com>
2018-02-25 21:26:09 +0800

    adjust code style

--- incubator.go
+++ incubator.go
@@ -20,12 +20,12 @@ func (o *incubator) handleSignal(c chan os.Signal) {
 	var err error
 	for {
 		s := <-c
-		log.Printf("signal received: %d\n", s)
+		log.Printf("Signal received: %d\n", s)
 		switch s {
 		case syscall.SIGUSR1:
 			err = o.app.OnReload()
 			if err != nil {
-				log.Fatalf("app OnReload failed: %s", err)
+				log.Fatalf("App OnReload failed: %s", err)
 			}
 		case syscall.SIGUSR2:
 			fallthrough
@@ -33,8 +33,8 @@ func (o *incubator) handleSignal(c chan os.Signal) {
 			fallthrough
 		case syscall.SIGTERM:
 			o.app.OnStop()
-			// 从此线程退出,认为是非正常退出
 			os.Exit(1)
+			// If the whole process exits from here, then something is wrong 
 		}
 	}
 }
@@ -43,7 +43,7 @@ func (o *incubator) incubate() {
 
 	opts, err := docopt.Parse(o.app.GetUsage(), nil, true, o.app.GetVersion(), false)
 	if err != nil {
-		log.Fatalf("parse cmd options failed: %s", err)
+		log.Fatalf("Parse cmd options failed: %s", err)
 	}
 
 	o.app.OnOptParsed(opts)
